0bbb6636ba81655369c2efd4b1333f8eaef31a7b
Bun43 <bunroeunchhum@gmail.com>
2022-07-16 21:48:44 +0200

    php

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Bunroeun CHHUM CV</title>
+</head>
+
+<body>
+    <div class="page">
+        <div class="en-tete col">
+            <h1>bunroeun chhum</h1>
+            <h3>développeur web junior</h3>
+        </div>
+
+        <div class="ligne"></div>
+
+        <div class="corps col">
+            <div class="gauche col-6">
+                <div class="profil col">
+                    <h2>profil</h2>
+                    <p>Passionnée par les nouvelles
+                        technologies et désireux d'en
+                        connaitre toujours plus.
+                        Le développement dans le web me
+                        permet de mettre en exergue les
+                        compétences acquises et
+                        d'apprendre constamment de
+                        nouvelles méthodes dans un
+                        domaine en constante évolution.</p>
+                </div>
+                <div class="coordonnees col">
+                    <h2>coordonnées</h2>
+                    <table>
+                        <tr>
+                            <th><i class="fa-solid fa-phone"></i></th>
+                            <td>06 51 98 17 23</td>
+                        </tr>
+                        <tr>
+                            <th><i class="fa-solid fa-at"></i></th>
+                            <td>bunroeunchhum@gmail.com</td>
+                        </tr>
+                        <tr>
+                            <th><i class="fa-solid fa-house"></i></th>
+                            <td>
+                                <p>HLM 1 appartement 5</p>
+                                <p>avenue Jean Bonnevialle</p>
+                                <p>43140 La Séauve sur Semène</p>
+                            </td>
+                        </tr>
+                    </table>
+                    <ul>
+                        <ol></ol>
+                        <ol></ol>
+                        <ol>
+
+                        </ol>
+                    </ul>
+                    <p></p>
+                    <p></p>
+                </div>
+                <div class="competences col">
+                    <h2>comptétences</h2>
+                    <table>
+                        <tr>
+                            <th>wordpress</th>
+                            <td><i class="fa-solid fa-battery-three-quarters"></i></td>
+                        </tr>
+                        <tr>
+                            <th>html css</th>
+                            <td><i class="fa-solid fa-battery-half"></i></i></td>
+                        </tr>
+                        <tr>
+                            <th>php</th>
+                            <td><i class="fa-solid fa-battery-half"></i></td>
+                        </tr>
+                        <tr>
+                            <th>javascript</th>
+                            <td><i class="fa-solid fa-battery-half"></i></td>
+                        </tr>
+                        <tr>
+                            <th>symfony</th>
+                            <td><i class="fa-solid fa-battery-half"></i></td>
+                        </tr>
+                        <tr>
+                            <th>react</th>
+                            <td><i class="fa-solid fa-battery-half"></i></td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <div class="droite col-6">
+                <div class="experience col">
+                    <h2>expérience profesionnelle</h2>
+                </div>
+                <div class="formation col">
+                    <h2>formation</h2>
+                </div>
+                <div class="loisirs col">
+                    <h2>loisirs</h2>
+                </div>
+            </div>
+        </div>
+        <div class="bas-de-page col">
+            <h1>3</h1>
+        </div>
+    </div>
+    <div class="footer fixed-bottom col-12">
+        <p>essai</p>
+    </div>
+</body>
+<script src="https://kit.fontawesome.com/ed1208b2df.js" crossorigin="anonymous"></script>
+
+
+</html>
